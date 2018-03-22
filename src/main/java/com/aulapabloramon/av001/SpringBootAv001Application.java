740b59a1f36c1fdbf9f58f7ba28b2f3244a6273c
yusefrich <yusef.rick@gmail.com>
2018-03-22 09:39:59 -0300

    primeira parte da avaliacao

---
+++ src/main/java/com/aulapabloramon/av001/SpringBootAv001Application.java
@@ -0,0 +1,12 @@
+package com.aulapabloramon.av001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAv001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAv001Application.class, args);
+	}
+}
